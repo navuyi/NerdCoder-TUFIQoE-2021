19512d78d559bc0e15b26e761de5e9ffa7c093ee
Rafal Figlus <rafal_figlus@wp.pl>
2021-06-30 18:05:13 +0200

    build push

---
+++ dist/background.js
@@ -0,0 +1,148 @@
+///////////////////////////////////////////////////////////////
+// DO NOT TRY ANY IMPORTS IN THIS FILE - DOES NOT WORK ! ! ! //
+///////////////////////////////////////////////////////////////
+
+
+const yt_watch_string = "https://www.youtube.com/watch?v=";
+var captured_data = [];
+
+// Initialize config values
+chrome.runtime.onInstalled.addListener( ()=>{
+    const config = {
+        ASSESSMENT_PANEL_OPACITY: 80,           // Opacity of the assessment panel in %
+        ASSESSMENT_INTERVAL_MS: 60000,           // Interval for assessment in auto mode in milliseconds
+        ASSESSMENT_MODE: "auto",                // Available modes are "remote", "auto" and "manual"
+        ASSESSMENT_PAUSE: "disabled"     // Enable/disable playback pausing/resuming on video assessment
+    };
+    chrome.storage.local.set(config, ()=>{
+        console.log("Config has been saved: " + config);
+    });
+});
+
+
+function submit_captured_data(captured_data, tabId){
+    const my_url = " http://127.0.0.1:5000/new_session";
+    const my_method = "POST";
+    const my_headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    };
+
+    const index = captured_data.findIndex(record => record.id === tabId);
+    console.log(index);
+    if(index !== -1){
+        const session_data = captured_data[index].data;
+        const assessment_data = captured_data[index].assessments;
+        const my_body = JSON.stringify({session_data: session_data, assessment_data: assessment_data});
+
+        // Delete sent data from captured_data array
+        captured_data.splice(index,1);
+
+        // Send data
+        fetch(my_url, {method: my_method, headers: my_headers, body: my_body})
+            .then(res => res.json())
+            .then(re => console.log(re));
+    }
+    else {
+        console.log("No data to submit");
+    }
+}
+
+// Listen for entering youtube page with video player
+// webNavigation.onHistoryStateUpdated is used instead of tabs.onUpdated due to multiple executions coused by iframes
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+    if(details.frameId === 0 && details.url.includes(yt_watch_string)) {
+        chrome.tabs.get(details.tabId, (tab) => {
+            if(tab.url === details.url) {
+
+                // If there is any captured data from last session submit it and clear the array
+                if(captured_data.length > 0){
+                    submit_captured_data(captured_data, details.tabId);
+                }
+                else {
+                    console.log("No data captured yet");
+                }
+                // Inject content script into current page with video player
+                chrome.tabs.executeScript(tab.id, {file: "init.js"});
+            }
+        });
+    }
+});
+
+// Listen for onCommittedd events and page reload in particular
+// onCommitted - part of the page content is loaded - inject content script
+chrome.webNavigation.onCommitted.addListener((details) => {
+    if(details.frameId === 0 && details.url.includes(yt_watch_string)){
+        chrome.tabs.get(details.tabId, (tab) => {
+            if(tab.url === details.url){
+                if (["reload", "typed", "link"].includes(details.transitionType) && details.url.includes(yt_watch_string)){
+                    // Inject content script into current page with video player
+                    chrome.tabs.executeScript(details.tabId, {file: "init.js"});
+                }
+            }
+        });
+    }
+
+});
+
+// Listen for tab updates
+chrome.tabs.onUpdated.addListener((tab_id, changeInfo, tab)=>{
+
+    // Listen for changing to pages different than youtube page with video player
+    if(changeInfo.status == "complete" && !tab.url.includes(yt_watch_string)){
+        // Send signal to stop capturing data
+        chrome.tabs.sendMessage(tab_id, {msg: "stop"});
+
+        // Submit captured data and clear array
+        submit_captured_data(captured_data, tab.id);
+    }
+});
+
+
+// Listen for messages from content script
+chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+
+        // Listen for handover data - captured nerd statistics
+        if(request.msg == "data_handover"){
+            const received_data = request.data;
+            const tabId = sender.tab.id;
+
+            // Look if data from this monitor session already exists
+            const record = captured_data.find(record => record.id === tabId);
+            if(record !== undefined){
+                record.data.push(received_data);
+            }
+            else {
+                const record = {
+                    id: tabId,
+                    data: [received_data]
+                };
+                captured_data.push(record);
+            }
+            sender.tab.id;
+        }
+        // Listen for assessment handover
+        if(request.msg == "assessment_handover"){
+            const received_assessment = request.data;
+            const tabId = sender.tab.id;
+
+            const record = captured_data.find(record => record.id === tabId);
+            if(record !== undefined){
+                if(record.assessments !== undefined){
+                    record.assessments.push(received_assessment);
+                }
+                else {
+                    Object.assign(record, {assessments: [received_assessment]});
+                }
+            }
+        }
+        // Listen for onbeforeunload message - tab close, refresh
+        else if(request.msg == "onbeforeunload"){
+            submit_captured_data(captured_data, sender.tab.id);
+
+            if(request.type == "video_end"){
+                chrome.tabs.sendMessage(sender.tab.id, {msg: "stop"});
+            }
+        }
+    }
+);
---
+++ dist/popup.js
@@ -0,0 +1,74 @@
+// Set the default configuration
+
+// Default for assessment panel opacity
+chrome.storage.local.get(["ASSESSMENT_PANEL_OPACITY"], (result)=> {
+    document.getElementById("opacity-input").value = result.ASSESSMENT_PANEL_OPACITY;
+});
+// Default for assessment interval
+chrome.storage.local.get(["ASSESSMENT_INTERVAL_MS"], (result)=> {
+    document.getElementById("interval-input").value = result.ASSESSMENT_INTERVAL_MS;
+});
+// Default for assessment mode - auto/remote/manual
+chrome.storage.local.get(["ASSESSMENT_MODE"], (result)=>{
+    const button_id = "mode-"+result.ASSESSMENT_MODE;
+    document.getElementById(button_id).setAttribute("active", true);
+});
+
+
+// Assessment mode - buttons configuration
+const buttons = ["mode-remote", "mode-auto", "mode-manual"];
+const handle_mode_change = (e) => {
+    // Get id of the clicked button
+    const clicked_button_id = e.target.id;
+    // Remove active attribute from all buttons
+    buttons.forEach((id)=>{
+        document.getElementById(id).removeAttribute("active");
+    });
+    // Set active attribute to last clicked button
+    document.getElementById(clicked_button_id).setAttribute("active", true);
+
+    // Change the assessment mode in chrome storage
+    const mode = clicked_button_id.match(/mode-([a-zA-Z]+)/)[1];
+    chrome.storage.local.set({ASSESSMENT_MODE: [mode]});
+};
+// To every mode button attach event listener
+buttons.forEach((id, index)=>{
+    document.getElementById(id).addEventListener("click", handle_mode_change);
+});
+
+
+
+
+
+
+
+// Handle configuration save
+document.getElementById("save-button").addEventListener('click', (e)=>{
+    // Handle opacity save
+    let opacity = document.getElementById("opacity-input").value;
+    if(opacity>100){
+        opacity=100;
+        document.getElementById("opacity-input").value = 100;
+    }
+    else if(opacity<0){
+        opacity = 0;
+        document.getElementById("opacity-input").value = 0;
+    }
+
+    // Handle interval change
+    let interval = document.getElementById("interval-input").value;
+    if(interval<0){
+        interval = 1000;
+        document.getElementById("interval-input").value = 1000;
+    }
+
+    const new_config = {
+        ASSESSMENT_PANEL_OPACITY: [opacity],
+        ASSESSMENT_INTERVAL_MS: [interval]
+    };
+    chrome.storage.local.set(new_config, ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+        });
+    });
+});
